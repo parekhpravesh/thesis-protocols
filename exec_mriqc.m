5b951a3a1f29a6f900d3243fd6e6c33e67b962da
parekhpravesh <praveshparekh@gmail.com>
2017-02-14 12:46:55 +0530

    Initial commit

---
+++ exec_mriqc.m
@@ -0,0 +1,13 @@
+function exec_mriqc
+% Executes mriqc and writes the log to a text file
+
+input_dir = '/run/media/MBIAL/MBIAL_STORAGE/Parekh/mriqc_dst_25Jan2017_BIDS/';
+output_dir = '/run/media/MBIAL/MBIAL_STORAGE/Parekh/mriqc_dst_25Jan2017_BIDS_Output';
+work_dir = '/run/media/MBIAL/MBIAL_STORAGE/Parekh/mriqc_dst_25Jan2017_BIDS_Work';
+log_name = fullfile(output_dir, 'mriqc_log.txt');
+
+system_string = ['"mriqc ', input_dir, ' ', output_dir, ...
+    ' --verbose-reports --n_procs 30 participants -w ', work_dir, ' > ', ...
+    log_name, '"'];
+
+system(system_string);
